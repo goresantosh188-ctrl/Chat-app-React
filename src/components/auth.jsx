188d53e237ba28ef87c82f3674ee47ad9959adbd
Smaran Gore <goresantosh188@gmail.com>
2025-09-15 21:25:59 +0530

    Added username cookie

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -107,6 +107,7 @@ function Auth({ setIsAuth }) {
                 setIsAuth(true);
                 window.alert(`User logged in \n Username: ${username} \n Email: ${email} \n Password: ${password}`);
                 cookies.set("auth-token", userCredentials.user.refreshToken);
+                cookies.set("username", username);
                 cookies.set("email", userCredentials.user.email);
                 cookies.set("password", password);
             } catch (error) {
