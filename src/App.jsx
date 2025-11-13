1001e1662429d2fe6eb384545395c5f1daabe3fa
Smaran Gore <goresantosh188@gmail.com>
2025-11-13 18:36:44 +0530

    Added favicon and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Auth from "./components/auth.jsx";
 import EnterRoom from "./components/rooms.jsx";
 import { cookies } from "./global/config.js";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const username = cookies.get("username");
   console.log(username);
+
+  useEffect(() => {
+    document.title = "Chat App"
+  }, [])
+
   if (!isAuth) {
     return(<> 
       <Auth setIsAuth={setIsAuth}/> 
